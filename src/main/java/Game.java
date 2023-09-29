12c094a7f4aba3ed7b7e79dd611d4a346f5feff5
charchalis <charchalis97@gmail.com>
2023-09-29 11:18:08 +0100

    created game class

---
+++ src/main/java/Game.java
@@ -0,0 +1,39 @@
+import com.googlecode.lanterna.TerminalSize;
+import com.googlecode.lanterna.TextCharacter;
+import com.googlecode.lanterna.screen.Screen;
+import com.googlecode.lanterna.screen.TerminalScreen;
+import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
+import com.googlecode.lanterna.terminal.Terminal;
+
+import java.io.IOException;
+
+public class Game {
+
+    private Screen screen;
+    private int x = 10;
+    private int y = 10;
+
+    public Game() throws IOException {
+        TerminalSize terminalSize = new TerminalSize(40, 20);
+        DefaultTerminalFactory terminalFactory = new DefaultTerminalFactory().setInitialTerminalSize(terminalSize);
+        Terminal terminal = terminalFactory.createTerminal();
+
+        Screen screen = new TerminalScreen(terminal);
+        screen.setCursorPosition(null);   // we don't need a cursor
+        screen.startScreen();             // screens must be started
+        screen.doResizeIfNecessary();     // resize screen if necessary
+
+
+    }
+
+    private void draw() throws IOException {
+        screen.clear();
+        screen.setCharacter(x, y, TextCharacter.fromCharacter('X')[0]);
+        screen.refresh();
+    }
+
+    public void run(){
+        draw();
+    }
+
+}
